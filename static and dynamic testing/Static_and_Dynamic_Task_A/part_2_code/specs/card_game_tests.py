7ade8644aa2faaab3db810af3b01213cf9f4e787
saimeerasingh <singhsaimeera@gmail.com>
2022-03-06 13:35:35 +0000

    first test written

--- static and dynamic testing/Static_and_Dynamic_Task_A/part_2_code/specs/card_game_tests.py
+++ static and dynamic testing/Static_and_Dynamic_Task_A/part_2_code/specs/card_game_tests.py
@@ -1,6 +1,16 @@
 import unittest
 from src.card import Card
-from src.card_game import CardGame
+from src.card_game import check_for_ace
 
-class TestCardGame:
-    pass
+class TestCardGame(unittest.TestCase):
+
+    def test_check_for_ace_if_true(self):
+        card = Card("heart",1)
+        assert check_for_ace(card) == True
+    
+    def test_check_for_ace_if_false(self):
+        card = Card("heart",2)
+        assert check_for_ace(card) == False
+
+
+    
