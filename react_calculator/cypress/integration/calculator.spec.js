c6b615110708908faafacad44ac5c74b54efecaa
saimeerasingh <singhsaimeera@gmail.com>
2022-03-24 19:57:26 +0000

    added cypress test

--- react_calculator/cypress/integration/calculator.spec.js
+++ react_calculator/cypress/integration/calculator.spec.js
@@ -7,4 +7,45 @@ describe("Calculator", () => {
     cy.get('#number2').click();
     cy.get('.display').should('contain', '2')
   })
+
+  it('should have running total', () => {
+    cy.get('#number2').click();
+    cy.get('#operator_add').click();
+    cy.get('#number3').click();
+    cy.get("#operator-equals").click();
+    cy.get('.display').should('contain', '5')
+  })
+
+  it('should do airthmatic operations', () => {
+    cy.get('#number3').click();
+    cy.get('#operator-subtract').click();
+    cy.get('#number2').click();
+    cy.get("#operator-equals").click();
+    cy.get('.display').should('contain', '1')
+  })
+
+  it('should do multiple operations chained togther', () => {
+    cy.get('#number3').click();
+    cy.get('#operator-subtract').click();
+    cy.get('#number2').click();
+    cy.get('#operator-multiply').click();
+    cy.get('#number1').click();
+    cy.get("#operator-equals").click();
+    cy.get('.display').should('contain', '1')
+  })
+
+  it('should do airthmatic operations on large decimal ', () => {
+    cy.get('#number3').click();
+    cy.get('#number3').click();
+    cy.get('#number3').click();
+    cy.get('#number3').click();
+    cy.get('#number3').click();
+    cy.get('#number3').click();
+    cy.get('#decimal').click();
+    cy.get('#number3').click();
+    cy.get('#operator-multiply').click();
+    cy.get('#number2').click();
+    cy.get("#operator-equals").click();
+    cy.get('.display').should('contain', '666666.6')
+  })
 })
